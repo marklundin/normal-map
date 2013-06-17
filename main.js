44d5fda1954f5e3aa7ede294e53e9bd72741317c
Mark Lundin <mark@mark-lundin.com>
2013-06-17 15:44:04 +0100

    Removed uneccessary test data

--- main.js
+++
@@ -1,9 +0,0 @@
-define( ['source/textureutils', 'libs/image!heightmap.png'], function( util, image ){
-	
-	util.heightToNormal( image, function( normalImage ){
-
-		document.body.appendChild( normalImage );
-
-	} );
-
-});
